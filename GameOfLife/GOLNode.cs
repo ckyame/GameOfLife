d6a26391c1e4b917d351f77802b846765a27b3f5
Chris <Christopher.kyame@laverne.edu>
2017-10-13 02:26:57 -0700

    init

---
+++ GameOfLife/GOLNode.cs
@@ -0,0 +1,193 @@
+﻿using System.Linq;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Collections.Generic;
+
+namespace GameOfLife
+{
+    public class GOLNode
+    {
+        public enum NodeAction
+        {
+            Waiting,    // For idle nodes and inbetween processing steps
+            Die,        // Node will die
+            StayAlive,  // Node will stay alive
+            Populate    // Node will become alive
+        }
+
+        public static List<GOLNode> Map = new List<GOLNode>();
+
+        public static int LBFDP = 1; // Requirement for de populization
+        public static int UBFDP = 4; // Requirement for de populization
+        public static int SBFL1 = 2; // Requirement for living 1
+        public static int SBFL2 = 3; // Requirement for living 2
+        public static int RQFBA = 3; // Requirement for becoming alive
+
+        public Button Node { get; set; }
+
+        public string Name { get; set; }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public bool IsTaken { get; set; }
+     
+        private NodeAction NextAction;
+        private int StepsWhileDeadCount = 0;
+
+        private Brush DefaultBrush;
+
+        #region Ctor
+
+        /// <summary>
+        /// Constructor saves button in variable, sets background to black, and is added to map
+        /// </summary>
+        /// <param name="node"></param>
+        public GOLNode(Button node)
+        {
+            Node = node;
+            DefaultBrush = Brushes.Cyan;
+            Node.Background = Brushes.Black;
+            Map.Add(this);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Sets the background to limegreen and becomes taken
+        /// </summary>
+        public void TakeNode()
+        {
+            Node.Background = Brushes.LimeGreen;
+            IsTaken = true;
+        }
+        /// <summary>
+        /// Sets background to default brush and is no longer taken
+        /// </summary>
+        public void ResetNode()
+        {
+            Node.Background = DefaultBrush;
+            IsTaken = false;
+        }
+        /// <summary>
+        /// Gets the nieghbor count of this node
+        /// </summary>
+        /// <returns></returns>
+        public int NeighborCount()
+        {
+            int count = 0;
+            GOLNode tl = GOLNode.Map.FirstOrDefault(n => n.X == X - 1 && n.Y == Y + 1);
+            GOLNode tt = GOLNode.Map.FirstOrDefault(n => n.X == X - 0 && n.Y == Y + 1);
+            GOLNode tr = GOLNode.Map.FirstOrDefault(n => n.X == X + 1 && n.Y == Y + 1);
+            GOLNode rr = GOLNode.Map.FirstOrDefault(n => n.X == X + 1 && n.Y == Y + 0);
+            GOLNode br = GOLNode.Map.FirstOrDefault(n => n.X == X + 1 && n.Y == Y - 1);
+            GOLNode bb = GOLNode.Map.FirstOrDefault(n => n.X == X + 0 && n.Y == Y - 1);
+            GOLNode bl = GOLNode.Map.FirstOrDefault(n => n.X == X - 1 && n.Y == Y - 1);
+            GOLNode ll = GOLNode.Map.FirstOrDefault(n => n.X == X - 1 && n.Y == Y - 0);
+            if (tl != null && tl.IsTaken) count++;
+            if (tt != null && tt.IsTaken) count++;
+            if (tr != null && tr.IsTaken) count++;
+            if (rr != null && rr.IsTaken) count++;
+            if (br != null && br.IsTaken) count++;
+            if (bb != null && bb.IsTaken) count++;
+            if (bl != null && bl.IsTaken) count++;
+            if (ll != null && ll.IsTaken) count++;
+            return count;
+        }
+        /// <summary>
+        /// Sets the action of the node for the next processing step
+        /// </summary>
+        /// <param name="action"></param>
+        public void SetNextAction(NodeAction action)
+        {
+            NextAction = action;
+        }
+        /// <summary>
+        /// Does the set NodeAction
+        /// </summary>
+        public void DoNextAction()
+        {
+            switch (NextAction)
+            {
+                case NodeAction.Die:
+                    {
+                        KillNode();
+                        SetNextAction(NodeAction.Waiting);
+                        StepsWhileDeadCount = 0;
+                        break;
+                    }
+                case NodeAction.Populate:
+                    {
+                        TakeNode();
+                        break;
+                    }
+                case NodeAction.StayAlive:
+                    {
+                        SetNextAction(NodeAction.Waiting);
+                        break;
+                    }
+            }
+            if (IsTaken == false && Node.Background == Brushes.PaleVioletRed)
+            {
+                StepsWhileDeadCount += 1;
+                if (StepsWhileDeadCount > 8)
+                {
+                    Node.Background = Brushes.Black;
+                    StepsWhileDeadCount = 0;
+                }
+            }
+        }
+        /// <summary>
+        /// Game Of Life Ruling Function 
+        /// This function can change this node
+        /// </summary>
+        public void NodeRuling()
+        {
+            if (IsTaken)
+            {
+                int nnc = NeighborCount();
+                if (nnc >= UBFDP || nnc <= LBFDP)
+                {
+                    SetNextAction(NodeAction.Die);
+                }
+                if (nnc == SBFL1 || nnc == SBFL2)
+                {
+                    SetNextAction(NodeAction.StayAlive);
+                }
+            }
+            else
+            {
+                if (NeighborCount() == RQFBA)
+                {
+                    SetNextAction(NodeAction.Populate);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Sets the background to red and is no longer taken
+        /// </summary>
+        private void KillNode()
+        {
+            Node.Background = Brushes.PaleVioletRed;
+            IsTaken = false;
+        }
+        /// <summary>
+        /// Sets all the nodes to a waiting state for the next processing step
+        /// </summary>
+        private static void SetForNextTurn()
+        {
+            GOLNode.Map.ForEach(node => node.DoNextAction());
+            GOLNode.Map.ForEach(node => node.SetNextAction(GOLNode.NodeAction.Waiting));
+            GOLNode.Map.ForEach(node => node.DoNextAction());
+        }
+
+        #endregion 
+    }
+}
---
+++ GameOfLife/MainWindow.xaml.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using System.Collections.Generic;
+
+namespace GameOfLife
+{
+    public partial class MainWindow : Window
+    {
+        private List<GOLNode> Nodes = new List<GOLNode>();
+
+        private string InputErrorTitle = Properties.Resources.AliveNodeLabelText;
+        private string InputErrorMessage = Properties.Resources.InputErrorMessage;
+        private string AliveNodeLabelText = Properties.Resources.AliveNodeLabelText;
+        private string LargestPopLabelText = Properties.Resources.LargestPopLabelText;
+        private string TotalStepsLabelText = Properties.Resources.TotalStepsLabelText;
+
+        private Action EmptyAction = new Action(() => { });
+
+        #region Constructor 
+
+        /// <summary>
+        /// Dispatcher Invoke here to know when all child components have been loaded
+        /// and to be able to call NodeSetup
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            Dispatcher.BeginInvoke(DispatcherPriority.Loaded, new Action(() => NodesSetup()));
+        }
+        /// <summary>
+        /// Sets up each node on the map
+        /// </summary>
+        private void NodesSetup()
+        {
+            int rM = 52;
+            int r = 0;
+            int cM = 51;
+            int c = 0;
+            foreach (Button node in FindVisualChildren<Button>(this).Where(b => String.IsNullOrEmpty(b.Name)))
+            {
+                node.Name = string.Format("N_{0}_{1}", r, c);
+                node.Click += Node_Click;
+                node.MouseEnter += Node_Hover;
+                Nodes.Add(new GOLNode(node)
+                {
+                    Name = node.Name,
+                    X = c,
+                    Y = r
+                });
+                if (++c > 51)
+                {
+                    c = 0;
+                    ++r;
+                }
+            }
+        }
+
+        #endregion
+
+        #region UI Event Handlers
+
+        /// <summary>
+        /// Starts Game Of Life
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void Start_Click(object sender, RoutedEventArgs e)
+        {
+            bool lbfdp = Int32.TryParse(LRFDPTextBox.Text, out GOLNode.LBFDP);
+            bool ubfdp = Int32.TryParse(UPFDPTextBox.Text, out GOLNode.UBFDP);
+            bool sbfl1 = Int32.TryParse(RTSA1TextBox.Text, out GOLNode.SBFL1);
+            bool sbfl2 = Int32.TryParse(RTSA2TextBox.Text, out GOLNode.SBFL2);
+            bool rqfba = Int32.TryParse(RTPOPTextBox.Text, out GOLNode.RQFBA);
+            int steps = 0;
+            if (lbfdp && ubfdp && sbfl1 && sbfl2 && rqfba)
+            {
+                BackgroundWorker worker = new BackgroundWorker();
+                worker.DoWork += (o, dwea) => GOLNode.Map.ForEach(node => node.NodeRuling());
+                for (;;)
+                {
+                    worker.RunWorkerAsync();
+                    while (worker.IsBusy)
+                    {
+                        Dispatcher.Invoke(DispatcherPriority.ContextIdle, EmptyAction);
+                    }
+                    steps += 1;
+                    GOLNode.Map.ForEach(node => node.DoNextAction());
+                    AliveNodesLabel.Content = string.Format(AliveNodeLabelText, GOLNode.Map.Count(n => n.IsTaken));
+                    LargestPopulationLabel.Content = string.Format(LargestPopLabelText, GOLNode.Map.Max(n => n.NeighborCount()));
+                    TotalStepsLabel.Content = string.Format(TotalStepsLabelText, steps);
+                    GOLNode.Map.ForEach(node => node.SetNextAction(GOLNode.NodeAction.Waiting));
+                    GOLNode.Map.ForEach(node => node.DoNextAction());
+                }
+            }
+            else
+            {
+                MessageBox.Show(InputErrorMessage, InputErrorTitle, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        /// <summary>
+        /// Sets a node to a certain state depending on its current state
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Node_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            GOLNode gnode = GOLNode.Map.Single(node => node.Name == button.Name);
+            if (!gnode.IsTaken)
+            {
+                gnode.TakeNode();
+            }
+            else
+            {
+                gnode.ResetNode();
+            }
+        }
+        /// <summary>
+        /// Does Node Click but only if the user holds down the left mouse button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Node_Hover(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Button button = sender as Button;
+                GOLNode gnode = GOLNode.Map.Single(n => n.Name == button.Name);
+                if (!gnode.IsTaken)
+                {
+                    gnode.TakeNode();
+                }
+                else
+                {
+                    gnode.ResetNode();
+                }
+            }
+        }
+        
+        #endregion
+
+        #region Static
+
+        /// <summary>
+        /// Helpers function to find components of a parent
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="depObj"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
+}
